4afc4b0da4afdbae239ce2daed5e241cf76bc7fa
Николай Вовк <n.vovk@zebrains.team>
2020-02-11 21:47:20 +0300

    Add storybook and 'components' folder

--- index.js
+++ index.js
@@ -2,8 +2,10 @@
  * @format
  */
 
-import {AppRegistry} from 'react-native';
-import App from './App';
-import {name as appName} from './app.json';
+import { AppRegistry } from 'react-native'
+import App from './src'
+import { name as appName } from './app.json'
+import StoryBook from './storybook'
 
-AppRegistry.registerComponent(appName, () => App);
+AppRegistry.registerComponent(appName, () => StoryBook)
+// AppRegistry.registerComponent(appName, () => App)
